401443f58e837f967ad6e4aeb20365da0ec89633
joaoliveira-eng <joao.acadoliveira@gmail.com>
2026-08-22 17:17:48 -0300

    Boomclock explodindo V2

--- gamejam/bomb_clock.gd
+++ gamejam/bomb_clock.gd
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-=======
 class_name CreeperBomb
 extends EnemyBase
 
@@ -161,4 +134,3 @@ func take_damage(amount: int, damage_source: Vector2) -> void:
 	# if is_fusing: explode(); return
 
 	super.take_damage(amount, damage_source)
->>>>>>> Stashed changes
